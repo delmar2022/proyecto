0648c5764cf936c426c2ab8467bbb5deba4a40b2
delmar2022 <softwysop@gmail.com>
2022-02-11 14:38:11 -0600

    Inicio de proyecto

---
+++ recibe_get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos GET</title>
+</head>
+<body>
+Nombre: <?php echo $_get['nombre'] ?> <br>
+    Usuario: <?php echo $_get['usuario'] ?> <br>
+    Password: <?php echo $_get['password'] ?> <br>
+    Password2: <?php echo $_get['password2'] ?> <br>
+    Email: <?php echo $_get['email'] ?> <br>
+</body>
+</html>
